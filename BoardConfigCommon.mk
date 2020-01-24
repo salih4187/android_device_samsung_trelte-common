fe27dd9993e04bb6407f94c371435a4fb176510e
Kim Le <elektroschmock78@googlemail.com>
2020-01-24 17:36:36 +0100

    trelte-common: Set cpu "runtime variant" to cortex-a53
    This makes bionic and various art tools that compile/runs cpu-specific
    code choose the cortex-a53 variant at runtime. It does not affect
    build-time compilation.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -13,6 +13,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
 # Platform
 TARGET_BOARD_PLATFORM := exynos5
