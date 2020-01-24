491e29e37dddd9cf0b4cd94e5fe151e4037adec9
Kim Le <elektroschmock78@googlemail.com>
2020-01-24 17:35:30 +0100

    trelte-common: Use generic arch variant
    Follow blueline which has not demonstrated any regressions
    after switching to a generic arch variant.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,7 +12,7 @@ BOARD_VENDOR := samsung
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := generic
 
 # Platform
 TARGET_BOARD_PLATFORM := exynos5
