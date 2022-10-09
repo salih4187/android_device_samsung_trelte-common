7784e338bea2a94999f6e7a78e8b4e4163b1e31f
Harsh <iamhp2k@gmail.com>
2022-10-09 12:43:09 -0600

    trelte-common: Drop audio.a2dp.default
     * Building from here:
       https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35

--- device-common.mk
+++ device-common.mk
@@ -42,7 +42,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
     audio.primary.universal5433 \
