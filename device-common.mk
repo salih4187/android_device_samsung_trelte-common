0948008ee071873404a2eb516b571026a3c172ea
Salih Çağlar <salihcaglar4187@gmail.com>
2025-09-21 13:10:22 +0300

    trelte-common: Use HAL 3 Supported Camera Provider

--- device-common.mk
+++ device-common.mk
@@ -78,8 +78,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
     camera.universal5433 \
     Snap
 
