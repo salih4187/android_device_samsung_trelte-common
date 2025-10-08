95bc01d615df1c30ddec71c15ed1881be641c297
Salih Çağlar <salihcaglar4187@gmail.com>
2025-10-08 20:35:22 +0300

    trelte-common: Add SM prefix to model names

--- init/init_trelte.cpp
+++ init/init_trelte.cpp
@@ -179,45 +179,45 @@ void vendor_load_properties() {
     
     if (bootloader.find("N910C") == 0) {
         set_build_fingerprint("treltexx", "trelte", "N910CXXU2DVG1");
-        model = "N910C";
+        model = "SM-N910C";
     } else if (bootloader.find("N910H") == 0) {
         set_build_fingerprint("tre3gxx", "tre3g", "N910HXXU2DVG2");
-        model = "N910H";
+        model = "SM-N910H";
     } else if (bootloader.find("N910U") == 0) {
         set_build_fingerprint("trhpltexx", "trhplte", "N910UXXU2DVG1");
-        model = "N910U";
+        model = "SM-N910U";
     } else if (bootloader.find("N910S") == 0) {
         set_build_fingerprint("trelteskt", "trelteskt", "N910SKSU2DVG1");
-        model = "N910S";
+        model = "SM-N910S";
     } else if (bootloader.find("N910L") == 0) {
         set_build_fingerprint("treltelgt", "treltelgt", "N910LKLU2DVG1");
-        model = "N910L";
+        model = "SM-N910L";
     } else if (bootloader.find("N910K") == 0) {
         set_build_fingerprint("treltektt", "treltektt", "N910KKTU2DVG1");
-        model = "N910K";
+        model = "SM-N910K";
     } else if (bootloader.find("N915S") == 0) {
         set_build_fingerprint("tbelteskt", "tbelteskt", "N915SKSU2DVG1");
-        model = "N915S";
+        model = "SM-N915S";
     } else if (bootloader.find("N915L") == 0) {
         set_build_fingerprint("tbeltelgt", "tbeltelgt", "N915LKLU2DVG1");
-        model = "N915L";
+        model = "SM-N915L";
     } else if (bootloader.find("N915K") == 0) {
         set_build_fingerprint("tbeltektt", "tbeltektt", "N915KKTU2DVG1");
-        model = "N915K";
+        model = "SM-N915K";
     } else if (bootloader.find("N916S") == 0) {
         set_build_fingerprint("tre3calteskt", "tre3calteskt", "N916SKSU2DVG1");
-        model = "N916S";
+        model = "SM-N916S";
     } else if (bootloader.find("N916L") == 0) {
         set_build_fingerprint("tre3caltelgt", "tre3caltelgt", "N916LKLU2DVG1");
-        model = "N916L";
+        model = "SM-N916L";
     } else if (bootloader.find("N916K") == 0) {
         set_build_fingerprint("tre3caltektt", "tre3caltektt", "N916KKTU2DVG1");
-        model = "N916K";
+        model = "SM-N916K";
     } else {
         LOG(ERROR) << __func__ << ": Coudn't indentify model! Setting Default Model";
         set_build_fingerprint("treltexx", "trelte", "N910CXXU2DVG1");
         lte_properties();
-        model = "N910C";
+        model = "SM-N910C";
     }
     
     /* Only set 3G For N910H */
