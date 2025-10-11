89be20c4952ade79b8ec3fb417ea8a87a35f2b1b
Salih Çağlar <salihcaglar4187@gmail.com>
2025-10-11 12:25:48 +0300

    Revert "trelte-common: Use HAL 3 Supported Camera Provider"
    This reverts commit 0948008ee071873404a2eb516b571026a3c172ea.

--- device-common.mk
+++ device-common.mk
@@ -77,8 +77,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
     camera.universal5433 \
     Snap
 
