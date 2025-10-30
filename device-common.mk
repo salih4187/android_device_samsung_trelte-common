33cf56639c1c8711f9d1c56ad2a5a280787fb462
Salih Çağlar <salihcaglar4187@gmail.com>
2025-10-30 17:29:39 +0300

    trelte-common: Use passthrough mode for camera

--- device-common.mk
+++ device-common.mk
@@ -78,7 +78,6 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
-    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl-legacy \
     camera.universal5433 \
     Snap
