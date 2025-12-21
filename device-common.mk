72f80c5024904f36597927aa7005ed54cd266fcc
Salih Çağlar <salihcaglar4187@gmail.com>
2025-12-21 19:50:02 +0300

    Revert "Revert "trelte-common: Remove DRM's Widevine and Clearkey HIDL support""
    This reverts commit 0c228e52d7a97f554f6662b9bfe763dc06c47ff9.

--- device-common.mk
+++ device-common.mk
@@ -106,8 +106,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.0-service
 
 # Fingerprint
 PRODUCT_PACKAGES += \
