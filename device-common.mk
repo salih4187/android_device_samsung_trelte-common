fe291bdfb113d0096596a4a8a11808b8fed31122
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-07 00:41:17 +0300

    trelte-common: Switch to AIDL Power HAL

--- device-common.mk
+++ device-common.mk
@@ -272,7 +272,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.exynos
+    android.hardware.power-service.exynos
 
 # Ramdisk
 PRODUCT_PACKAGES += \
