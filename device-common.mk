88525bc189d8fd6d1dbe09ab3af9ffe753cad0b9
AKoskovich <akoskovich@pm.me>
2026-05-09 12:57:24 +0300

    trelte-common: Disable the usage of ConfigStore.  * As per commit LineageOS/android_hardware_interfaces@11a113a ("Add disable_configstore").
    [SebaUbuntu] Remove manifest entry as well
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- device-common.mk
+++ device-common.mk
@@ -81,8 +81,7 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.1-impl \
-    android.hardware.configstore@1.1-service
+    disable_configstore
 
 # Display
 #PRODUCT_PACKAGES += \
