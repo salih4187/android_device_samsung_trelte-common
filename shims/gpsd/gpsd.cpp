8cdd208e43eecfeb2d1dc9a59dd8a222f77ecd14
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-11 15:53:26 +0300

    trelte-common: Update GPSD Shim for S Based on: https://github.com/salih4187/android_device_samsung_universal5420-common/blob/lineage-20.0/libshims/libshim_gpsd/libshim_gpsd.cpp

--- shims/gpsd/gpsd.cpp
+++ shims/gpsd/gpsd.cpp
@@ -14,18 +14,36 @@
  * limitations under the License.
  */
 
-#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-extern "C" {
-  ssize_t _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list);
+namespace android {
+    extern "C" ssize_t _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list);
 
-  ssize_t _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list) {
-    return _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(thiz, list);
-  }
-  extern void _ZN7android13SensorManager16createEventQueueENS_7String8Ei(void **retVal, void *sensorMgr, void **str8P, int mode);
-}
+    extern "C" ssize_t _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list) {
+      return _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(thiz, list);
+    }
 
-extern "C" int _ZN7android13SensorManagerD1Ev();
-extern "C" int _ZN7android13SensorManagerC1Ev() {
-    return _ZN7android13SensorManagerD1Ev();
+    //android::String16::String16(char const*)
+    extern "C" void _ZN7android8String16C1EPKc(void **str16P, const char *str);
+
+    //android::String16::~String16()
+    extern "C" void _ZN7android8String16D1Ev(void **str16P);
+
+    extern "C" void _ZN7android13SensorManager16createEventQueueENS_7String8EiNS_8String16E(void **retVal, void *sensorMgr, void **str8P, int mode, void **str16P);
+
+    extern "C" void _ZN7android13SensorManager16createEventQueueENS_7String8Ei(void **retVal, void *sensorMgr, void **str8P, int mode)
+    {
+        void *string;
+
+        _ZN7android8String16C1EPKc(&string, "");
+        _ZN7android13SensorManager16createEventQueueENS_7String8EiNS_8String16E(retVal, sensorMgr, str8P, mode, &string);
+        _ZN7android8String16D1Ev(&string);
+    }
+
+    extern "C" int _ZN7android13SensorManagerD1Ev();
+    extern "C" int _ZN7android13SensorManagerC1Ev() {
+      return _ZN7android13SensorManagerD1Ev();
+    }
 }
