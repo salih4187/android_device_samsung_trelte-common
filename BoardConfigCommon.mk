a8d3ddd225499f451718bbfc338fa9515bcbf45c
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-14 13:25:47 +0300

    Revert "trelte-common: Enable Sepolicy"
    This reverts commit 2ffaf708b1272b2213b396b69c4437642b2f2bb1.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,7 +173,7 @@ BOARD_HAS_DOWNLOAD_MODE := true
 BOARD_RAMDISK_USE_XZ := true
 
 # SELinux
-BOARD_SEPOLICY_DIRS += device/samsung/trelte-common/sepolicy
+#BOARD_SEPOLICY_DIRS += device/samsung/trelte-common/sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Sensors
